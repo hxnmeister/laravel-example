0f35b4bc7f0c92173b70a599c814ffcb57a9c143
Valerii <valeriy.stronskiy@gmail.com>
2023-10-10 20:48:37 +0300

    Forms, post

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,22 +1,45 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+    use Illuminate\Http\Request;
+    use Illuminate\View\View;
 
-class MainController extends Controller
-{
-    function index()
+    class MainController extends Controller
     {
-        $title = 'Main Page';
-        $subtitle = '<em>Subtitle</em>';
-        $users = ['Tom', 'Bill', 'Mike'];
+        function index(): View
+        {
+            $title = 'Main Page';
+            $subtitle = '<em>Subtitle</em>';
+            $users = ['Tom', 'Bill', 'Mike'];
 
-        return view('index', compact('title', 'subtitle', 'users'));
-    }
+            return view('index', compact('title', 'subtitle', 'users'));
+        }
 
-    function contacts()
-    {
-        return view('contacts');
+        function contacts(): View
+        {
+            return view('contacts');
+        }
+
+        function sendEmail(Request $request)
+        {
+            $request->validate
+            ([
+                'name' => 'required|min:2|max:15',
+                'email' => 'required|email',
+                'message' => 'required|min:5'
+            ]);
+            // dd($request->name);
+
+            $name = $request->name;
+            $email = $request->email;
+            $message = $request->message;
+
+            mail('valeriy.stronskiy@gmail.com', 'Test_Subject', "$name, $email, $message");
+
+            // return redirect('/contacts')->with('success', 'Thank you!'); по url
+            // return redirect()->route('contacts')->with('success', 'Thank you!'); по назві маршрута
+            //return to_route('contacts')->with('success', 'Thank you!'); по назві маршрута
+            return redirect()->back()->with('success', 'Thank you!'); // повернення на попередню сторінку
+        }
     }
-}
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -2,18 +2,50 @@
 @section('content')
     <h1>Contacts</h1>
 
-    <form action="" method="post">
+    {{-- @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif --}}
+    
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{session('success')}}
+        </div>
+    @endif
+
+    <form action="{{route('sendEmail')}}" method="post">
+        @csrf
         <div>
             <label for="name">Name:</label>
-            <input type="text" name="name" id="name" class="form-control">
+            <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
+            @error('name')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
         <div>
             <label for="email">Email:</label>
-            <input type="email" name="email" id="email" class="form-control">
+            <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror" value="{{old('email')}}">
+            @error('email')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
         <div>
             <label for="message">Message:</label>
-            <textarea name="message" id="message" cols="30" rows="4" class="form-control"></textarea>
+            <textarea name="message" id="message" cols="30" rows="4" class="form-control @error('message') is-invalid @enderror">{{old('message')}}</textarea>
+            @error('message')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <button class="btn btn-primary mt-3">Send</button>
